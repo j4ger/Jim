55270eac2a72ff7a3c7348c46d3a4464cbcae102
j4ger <xiayuxuan@live.com>
2021-10-02 02:23:43 +0800

    Add bottom navigation layout Add daily log

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,51 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="navigationBar">
+    <div class="navigationButton">
+      <img src="@sicons/fa/MailBulk.svg" alt="对话" class="navigationIcon" />
+    </div>
+    <div class="navigationButton">
+      <img
+        src="@sicons/fa/AddressBook.svg"
+        alt="联系人"
+        class="navigationIcon"
+      />
+    </div>
+    <div class="navigationButton">
+      <img
+        src="@sicons/fa/LaughRegular.svg"
+        alt="我的"
+        class="navigationIcon"
+      />
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="stylus">
+$disabled-grey=#9e9e9e;
+
+.navigationBar {
+  margin:0;
+  display:flex;
+  height:50px;
+  width:100%;
+  position:fixed;
+  bottom:0;
+  left:0;
+  justify-content:space-evenly;
+  border-top:1px solid $disabled-grey;
+}
+.navigationButton {
+  width:50px;
+  display:flex;
+  justify-content:center;
+  padding-top:10px;
+}
+.navigationIcon {
+  width:30px;
+  height:30px;
 }
 </style>
+
+<script setup lang="ts">
+console.log("hello world");
+</script>
