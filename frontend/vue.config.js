6fd8e383e5bbaf29369c9cfd07efa47f39c06eb5
j4ger <xiayuxuan@live.com>
2021-10-07 02:01:57 +0800

    Fix typos;

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/Jim/docs/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/Jim/" : "/",
   outputDir: "../docs",
 };
